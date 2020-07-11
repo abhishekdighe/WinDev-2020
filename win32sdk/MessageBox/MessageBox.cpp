d78c2c1886d6282dd7c07317dc5b93e14cdbd115
Abhishek Dighe <abhishekdighe2598@gmail.com>
2020-07-11 17:00:12 +0530

    First Update

---
+++ win32sdk/MessageBox/MessageBox.cpp
@@ -0,0 +1,69 @@
+#include<windows.h>
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	WNDCLASSEX wndclass;
+	TCHAR szAppName[] = TEXT("app name");
+	HWND hwnd;
+	MSG msg;
+
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+
+	RegisterClassEx(&wndclass);
+
+	hwnd = CreateWindow(szAppName,
+		TEXT("My Message Box"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return(int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (iMsg)
+	{
+	case WM_CREATE:
+		MessageBox(hwnd, TEXT("create box"), TEXT("Message for create box"), 0);
+		break;
+	case WM_KEYDOWN:
+		MessageBox(hwnd, TEXT("key down"), TEXT("Message for key down"), 0);
+		break;
+	case WM_LBUTTONDOWN:
+		MessageBox(hwnd, TEXT("left click"), TEXT("Message for left button click"), 0);
+		break;
+	case WM_RBUTTONDOWN:
+		MessageBox(hwnd, TEXT("right click"), TEXT("Message for right button click"), 0);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return (DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
---
+++ win32sdk/MyNewWindow/Source.cpp
@@ -0,0 +1,76 @@
+//Headers
+#include<windows.h>
+
+//global function declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+//	|					  |		|		|		|
+//	LONG				HANDLE UINT	  word	   LONG
+//	|					  |		|		|_		|
+//	long(of c)			UINT unsignedint  |	  long(of c)
+//						 |				UINT
+//				unsignedint				  unsignedint
+// callback are the re-entrant functions i.e. deligates in .net and listner and adapter in java
+//only body is written by programmer of call back function.
+//WinMain()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	//variable declarations
+	WNDCLASSEX wndclass;//here WNDCLASSEX is struct
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MYAPP");
+
+	//code
+	//initialization of wndclassex
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;//here rhs side are macros
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;// only address is mention over here not called
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	//register above class
+	RegisterClassEx(&wndclass);
+
+	//create Window
+	hwnd = CreateWindow(szAppName,
+		TEXT("My Application"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	//message loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//code
+	switch (iMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
